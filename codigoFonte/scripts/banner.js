5e310170231adf1d963a9a68091bb5622e22afe3
Eduardo Goncalves Sena <112581981+EduardoDev94@users.noreply.github.com>
2023-05-12 12:13:37 -0300

    Adicionando Glider

--- codigoFonte/scripts/banner.js
+++ codigoFonte/scripts/banner.js
@@ -37,7 +37,46 @@
       }
     ]
   })
+  new Glider(document.querySelector('.glider-2'), {
+    // Mobile-first defaults
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    draggable:true,
+    scrollLock: true,
+    dots: '#resp-dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    
+    
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 380,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },{
+        // screens greater than >= 1024px
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+    ]
+  })
+
+
 })
+
 
 /*********************************************************/
 
