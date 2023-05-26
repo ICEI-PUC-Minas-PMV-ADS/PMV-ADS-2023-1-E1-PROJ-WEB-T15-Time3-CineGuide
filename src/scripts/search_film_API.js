c1082b7914a7617680415d63f177c5bef7d0eef8
Eduardo Goncalves Sena <112581981+EduardoDev94@users.noreply.github.com>
2023-05-26 18:18:05 -0300

    Configurando Acordeon

--- src/scripts/search_film_API.js
+++ src/scripts/search_film_API.js
@@ -17,32 +17,8 @@ const API_CONFIG = {
     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDJlNGEwY2I0ZmQ0ZDkzYWNmYjJmNmQ0MDRlZDVmOCIsInN1YiI6IjY0MDBiZjIzYzcxNzZkMDBkYjU5ZTZiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WmjBboz8g7fdLbqCSLCwYn2tZkakAQrrbsTn0N1udD0',
     'Content-Type': 'application/json; charset=utf-8'
   }
-  // Funcionalidades Acordeon
 }
-function toggleAccordion(header) {
-  var content = header.nextElementSibling;
-  var accordion = header.parentNode;
 
-  // Fecha todos os itens do acordeão
-  var items = accordion.getElementsByClassName('accordion-item');
-  for (var i = 0; i < items.length; i++) {
-    var item = items[i];
-    var itemContent = item.getElementsByClassName('accordion-content')[0];
-    if (itemContent.style.display === 'block') {
-      itemContent.style.display = 'none';
-      item.getElementsByClassName('accordion-header')[0].classList.remove('active');
-    }
-  }
-
-  // Abre ou fecha o item atual
-  if (content.style.display === 'block') {
-    content.style.display = 'none';
-    header.classList.remove('active');
-  } else {
-    content.style.display = 'block';
-    header.classList.add('active');
-  }
-}
 
 async function searchFilmByName() {
   main.innerHTML = '';
@@ -60,25 +36,46 @@ async function searchFilmByName() {
         <h3>${result.title}<h3>
         <h4>${result.original_title}</h4>
         <h4> Nota: ${result.vote_average}/10 </h4>
-        <h4> Ano de Lançamento: ${result.release_date}
+        <h4> Ano de Lançamento: ${result.release_date} </h4>
         <div class="wrapper-favorite">
             <h4>Favoritar</h4>
             <span class="material-symbols-outlined"> favorite </span>
         </div>
-        <div class="accordion">
+      <div class="accordion">
         <div class="accordion-item">
-          <div class="accordion-header" onclick="toggleAccordion(this)">Item 1</div>
+         <div class="accordion-header">Item 1 </div>
           <div class="accordion-content">
             <p>${result.overview}</p>
-          </div>
-        </div>
-     </div>`
+           </div>
+         </div>
+      </div>
+    </div>`
 
       cards_container.insertAdjacentHTML('beforeend', film_card)
     }
-
-
-
   }
   )
 }
+
+// Funcionalidades Acordeon
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+  const content = item.querySelector('.accordion-content');
+
+  header.addEventListener('click', () => {
+    if (content.style.display == 'none') {
+      content.style.display = 'block';
+    } else {
+      content.style.display = 'none';
+    }
+
+    // Fecha os outros itens abertos
+    accordionItems.forEach(otherItem => {
+      if (otherItem !== item) {
+        otherItem.querySelector('.accordion-content').style.display = 'none';
+      }
+    });
+  });
+});
