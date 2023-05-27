c4f77e4966128435c7a2279738dc265791b3e6ae
Eduardo Goncalves Sena <112581981+EduardoDev94@users.noreply.github.com>
2023-05-27 14:48:14 -0300

    configurando cards
    configurando cards de busca

--- src/scripts/search_film_API.js
+++ src/scripts/search_film_API.js
@@ -20,7 +20,8 @@ const API_CONFIG = {
 }
 
 
-async function searchFilmByName() {
+async function searchFilmByName() 
+{
   main.innerHTML = '';
   const response = await fetch(`${FILMS_API_BASE_URL}/3/search/multi?query=${queryName.value}&include_adult=true&language=pt-BR&page=1`, API_CONFIG);
   const data = await response.json();
@@ -29,53 +30,31 @@ async function searchFilmByName() {
   cards_container.classList.add('films-cards-container')
   main.appendChild(cards_container)
 
-  data.results.forEach(result => {
-    if (result.backdrop_path != undefined) {
-      const film_card = `<div class="movie-card">
-        <img id="img-test" src="https://image.tmdb.org/t/p/w500/${result.backdrop_path}" alt="${result.title}">
-        <h3>${result.title}<h3>
-        <h4>${result.original_title}</h4>
-        <h4> Nota: ${result.vote_average}/10 </h4>
-        <h4> Ano de Lançamento: ${result.release_date} </h4>
-        <div class="wrapper-favorite">
-            <h4>Favoritar</h4>
-            <span class="material-symbols-outlined"> favorite </span>
-        </div>
-      <div class="accordion">
-        <div class="accordion-item">
-         <div class="accordion-header">Item 1 </div>
-          <div class="accordion-content">
-            <p>${result.overview}</p>
-           </div>
-         </div>
-      </div>
-    </div>`
+  data.results.forEach(result => 
+  {
+    if (result.backdrop_path === undefined || result.title === undefined) 
+    {
+      return;
+    }
+    else 
+    {
+      const film_card = document.createElement('div')
+      film_card.classList.add('movie-card')
+
+      const image = document.createElement('img')
+      image.src = `https://image.tmdb.org/t/p/w500/${result.backdrop_path}`
+      film_card.appendChild(image)
+
+      const title = document.createElement('h3')
+      title.textContent = result.title
+      film_card.appendChild(title)
+
+      
+
 
       cards_container.insertAdjacentHTML('beforeend', film_card)
     }
   }
   )
 }
-
-// Funcionalidades Acordeon
-const accordionItems = document.querySelectorAll('.accordion-item');
-
-accordionItems.forEach(item => {
-  const header = item.querySelector('.accordion-header');
-  const content = item.querySelector('.accordion-content');
-
-  header.addEventListener('click', () => {
-    if (content.style.display == 'none') {
-      content.style.display = 'block';
-    } else {
-      content.style.display = 'none';
-    }
-
-    // Fecha os outros itens abertos
-    accordionItems.forEach(otherItem => {
-      if (otherItem !== item) {
-        otherItem.querySelector('.accordion-content').style.display = 'none';
-      }
-    });
-  });
-});
+      
