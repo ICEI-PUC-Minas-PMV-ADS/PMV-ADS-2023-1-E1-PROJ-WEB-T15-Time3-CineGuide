76cd779ad49c326ff650e199ee083e35ad664fa1
Eduardo Sena <dudu94bhz@gmail.com>
2023-06-04 01:29:06 -0300

    Refatorando e estilzando cards

--- src/scripts/search_film_API.js
+++ src/scripts/search_film_API.js
@@ -4,10 +4,7 @@ let buttonSearch = document.querySelector('#search-icon')
 
 buttonSearch.addEventListener('click', searchFilmByName)
 
-
-
 const FILMS_API_BASE_URL = 'https://api.themoviedb.org'
-
 
 const FILMS_API_KEY = '8d2e4a0cb4fd4d93acfb2f6d404ed5f8'
 
@@ -19,7 +16,7 @@ const API_CONFIG = {
   }
 }
 
-function dateConvert (data) {
+function dateConvert(data) {
   const partesData = data.split('-');
   const ano = partesData[0];
   const mes = partesData[1];
@@ -28,8 +25,7 @@ function dateConvert (data) {
 }
 
 
-async function searchFilmByName() 
-{
+async function searchFilmByName() {
   main.innerHTML = '';
   const response = await fetch(`${FILMS_API_BASE_URL}/3/search/multi?query=${queryName.value}&language=pt-BR&region=BR&page=1`, API_CONFIG);
   const data = await response.json();
@@ -38,62 +34,50 @@ async function searchFilmByName()
   cards_container.classList.add('films-cards-container')
   main.appendChild(cards_container)
 
-  data.results.forEach(result => 
-  {
-    if (result.backdrop_path === undefined || result.title === undefined) 
-    {
+  data.results.forEach(result => {
+
+    if (result.backdrop_path === undefined || result.title === undefined) {
       return;
     }
-    else 
-    {
-     const film_card = document.createElement('div')
-     film_card.classList.add('movie-card')
+    else {
+      const film_card = document.createElement('div')
+      film_card.classList.add('movie-card')
+
+      const image = document.createElement('img')
+      image.src = `https://image.tmdb.org/t/p/w342/${result.poster_path}`
+      const title = document.createElement('h3')
+      title.textContent = result.title
+      const original_title = document.createElement('h5')
+      original_title.textContent = result.original_title
+      const release_date = document.createElement('h6')
+      release_date.textContent = `Lançamento: ${dateConvert(result.release_date)}`
+      
+      const note_favorite = document.createElement('div')
+      const film_note = document.createElement('h6')
+      note_favorite.classList.add('note-favorite')
+      const heartIcon = `<span class="material-symbols-outlined"> favorite </span>`
+      film_note.textContent = `Nota: ${parseFloat(result.vote_average.toFixed(2))}/10`
+      note_favorite.innerHTML = heartIcon
+      note_favorite.appendChild(film_note)
+      const sinopseButton = document.createElement('button')
+      sinopseButton.textContent = 'Ver mais'
      
-     const image = document.createElement('img')
-     image.src = `https://image.tmdb.org/t/p/w342/${result.poster_path}`
-     film_card.appendChild(image)
+      film_card.append(image,title,original_title,release_date,note_favorite,sinopseButton)
 
-     const title = document.createElement('h3')
-     title.textContent = result.title
-     film_card.appendChild(title)
+      sinopseButton.addEventListener('click', () => {
+        const card_modal = document.createElement('div')
+        card_modal.classList.add('modal-movie-card')
+        const body = document.querySelector('body')
+        body.appendChild(card_modal)
 
-     const original_title = document.createElement('h5')
-     original_title.textContent = result.original_title
-     film_card.appendChild(original_title)
-     
-     const release_date = document.createElement('p') 
-     release_date.textContent = `Lançamento: ${dateConvert(result.release_date)}`
-     film_card.appendChild(release_date)
+        fetch(`https://api.themoviedb.org/3/movie/${result.id}?language=pt-BR&api_key=FILMS_API_KEY`, API_CONFIG)
+          .then(response => response.json())
+          .then(data => {
+            const film = data;
+            console.log(film)
+            const genres = film.genres.map(genero => genero.name).join(', ')
 
-     const wrapper_favorite = document.createElement('div')
-     const film_note = document.createElement('p')
-     const voteFixed = parseFloat(result.vote_average.toFixed(2))
-     wrapper_favorite.classList.add('wrapper-favorite')
-     const heartIcon = `<span class="material-symbols-outlined"> favorite </span>`
-     film_note.textContent = `Nota: ${voteFixed}/10`
-     wrapper_favorite.innerHTML = heartIcon
-     wrapper_favorite.appendChild(film_note)  
-     film_card.appendChild(wrapper_favorite)         
-
-     const sinopseButton = document.createElement('button') 
-     sinopseButton.textContent = 'Ver mais'
-     film_card.appendChild(sinopseButton)
-    
-     sinopseButton.addEventListener('click', () =>{
-      const card_modal = document.createElement('div')
-      card_modal.classList.add('modal-movie-card')
-      const body = document.querySelector('body')
-      body.appendChild(card_modal)
-
-      fetch(`https://api.themoviedb.org/3/movie/${result.id}?language=pt-BR&api_key=FILMS_API_KEY`, API_CONFIG)
-     .then(response => response.json())
-     .then(data => {
-      const film = data;
-      console.log(film)
-      const genres = film.genres.map(genero => genero.name).join(', ')
-
-    
-      card_modal.innerHTML = `
+            card_modal.innerHTML = `
       <div class="modal-content">
         <img class="modal-poster" src="https://image.tmdb.org/t/p/w500/${result.backdrop_path}" alt="Movie Poster">
         <h2 class="modal-title">${result.title}</h2>
@@ -102,20 +86,19 @@ async function searchFilmByName()
         <span class="close">&times;</span>
         </div>
      </div> `
-    
 
-     const closeModalButton = document.querySelector('.close')
-     closeModalButton.addEventListener('click', () => {
-         card_modal.remove()
-     })
-    })
 
-     })         
-     cards_container.appendChild(film_card)
+            const closeModalButton = document.querySelector('.close')
+            closeModalButton.addEventListener('click', () => {
+              card_modal.remove()
+            })
+          })
+      })
+      cards_container.appendChild(film_card)
 
     }
   }
- )
+  )
 }
 
 /////// função para acessar generos:
@@ -140,14 +123,14 @@ fetch('https://api.themoviedb.org/3/movie/{result.id}?api_key=FILMS_API_KEY', AP
   })
   .catch(error => {
     console.error('Ocorreu um erro na requisição:', error);
-  });      */ 
-  async function getGenreFilm(filmID) {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${filmID}?language=pt-BR&api_key=FILMS_API_KEY`, API_CONFIG);
-    const data = await response.json();
-    
-    let film = await data;
-    const genres = film.genres.map(genero => genero.name).join(', ');
-    console.log(genres)
-    return genres;
-  }
- 
+  });      */
+async function getGenreFilm(filmID) {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${filmID}?language=pt-BR&api_key=FILMS_API_KEY`, API_CONFIG);
+  const data = await response.json();
+
+  let film = await data;
+  const genres = film.genres.map(genero => genero.name).join(', ');
+  console.log(genres)
+  return genres;
+}
+
