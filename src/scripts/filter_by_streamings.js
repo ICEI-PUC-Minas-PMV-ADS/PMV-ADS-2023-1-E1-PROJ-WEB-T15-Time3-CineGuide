0e327283b4fc3b8b8049e16d068f2057ef5a2ff5
Eduardo Goncalves Sena <112581981+EduardoDev94@users.noreply.github.com>
2023-06-07 18:23:00 -0300

    Configurando variáveis de generos e streaming

--- src/scripts/filter_by_streamings.js
+++ src/scripts/filter_by_streamings.js
@@ -1,3 +1,7 @@
+const type = document.getElementById('type')
+const streaming = document.getElementById('streaming')
+const genres = document.getElementById('genres')
+
 const FILMS_API_BASE_URL = 'https://api.themoviedb.org'
 
 const FILMS_API_KEY = '8d2e4a0cb4fd4d93acfb2f6d404ed5f8'
@@ -23,10 +27,11 @@ function dateConvert(data) {
   
   
   async function getByStreaming() {
-    const response = await fetch (`https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=pt-BR&page=1&region=BR&sort_by=popularity.desc&watch_region=BR&with_watch_providers=337`, API_CONFIG);
+    const response = await fetch (`https://api.themoviedb.org/3/discover/movie?include_adult=true&with_genres=${genres.value}&include_video=false&language=pt-BR&page=1&region=BR&sort_by=popularity.desc&watch_region=BR&with_watch_providers=${streaming.value}`, API_CONFIG);
     const data = await response.json();
     console.log(data)
     const cards_container = document.createElement('div')
     cards_container.classList.add('films-cards-container')
     main.appendChild(cards_container)
+    
 }
